363802ca7c1bce875928244a446697d33831cbac
Nidhi Nishad <91557785+nidhi-2619@users.noreply.github.com>
2022-11-09 23:46:30 +0530

    Update combinationSum.py

--- combinationSum.py
+++ combinationSum.py
@@ -4,6 +4,7 @@ class Solution:
         if index==len(arr):
             if target==0:
                 # copy is used so that new ds can be created else ds will take the reference only and no new ds will be created
+#                 the problem with my code was when index==n and target==0, I did ans.append(ds), the ans array is storing the reference of ds object. So, when ds changes in further recursions, the referenced  values in ans array also changes. So, I did ans.append(ds.copy()). Storing a copy for ds array so that I don’t lose that particular combination
                 ans.append(ds.copy())
             return 
 #  the intuition is pick and not pick the element 
