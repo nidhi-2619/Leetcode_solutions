815c4b71629d2c6b3e6a79a65be982e9127822f2
Nidhi Nishad <nidhinishad02@gmail.com>
2023-04-14 19:08:55 +0530

    Create Detect_cycle_in_a_undirected_graph.py
    This is a geeksforgeeks question ,I,ve included this here because it is the important part of the graph

---
+++ Graphs/Detect_cycle_in_a_undirected_graph.py
@@ -0,0 +1,45 @@
+
+from typing import List
+from collections import deque
+class Solution:
+    def detect(self,vis,adj,source):
+        vis[source]=True
+        q = deque()
+        # (source node,parent node)
+        q.append((source,-1))
+        while q:
+            node,parent = q.popleft()
+            for i in adj[node]:
+                if not vis[i]:
+                    vis[i]=True
+                    q.append((i,node))
+                # means it is in vis and the value of node is not equal to the parent 
+                # ie it has cycle
+                #  parent is checking with the current node to ensure that it is not the
+                #  previous node that we have visited but the new node which is already visited
+                #  before this node could visit
+                elif parent!=i:  
+                    return True
+        return False
+    
+    def dfs(self,vis,adj,node,parent):
+        vis[node]=True
+        for i in adj[node]:
+            if not vis[i]:
+                # if at any point it return false we get that it has cycle and we will return true
+                if self.dfs(vis,adj,i,node):
+                    return True
+            elif i!=parent:
+                return True
+        return False        
+        
+    #Function to detect cycle in an undirected graph.
+	def isCycle(self, V: int, adj: List[List[int]]) -> bool:
+		#Code here
+		vis = [False]*V
+# 		for all components
+		for i in range(V):
+		    if not vis[i]:
+		        if self.dfs(vis,adj,i,-1):
+		            return True
+		return False  
