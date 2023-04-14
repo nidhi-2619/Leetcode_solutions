9160cf4e5a9fe2ac7d1fd326e151f0f3db8d3c17
Nidhi Nishad <nidhinishad02@gmail.com>
2023-04-14 08:47:23 +0530

    Update Number_of_islands.py

--- Graphs/Number_of_islands.py
+++ Graphs/Number_of_islands.py
@@ -1,6 +1,5 @@
 
-# Question Link:
-# https://leetcode.com/problems/number-of-islands/  
+# [Question Link](https://leetcode.com/problems/number-of-islands/)  
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
